0e042246db6daa6b13f9bc5557f9f614f5ec7796
Melvin Wijaya <mewijaya@ucsd.edu>
2018-03-02 14:58:52 -0800

    EditProfile skeleton

---
+++ 134b/src/containers/Student/EditProfile.js
@@ -0,0 +1,26 @@
+import React, from 'react';
+import StudentAPI from '../Student/StudentAPI';
+
+class EditForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {value: ''};
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }
+}
