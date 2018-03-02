b4185b2a29aaa6c19f5a4f3548565be7240f5138
Kevin Pansawira <kpansawi@ucsd.edu>
2018-03-01 19:25:35 -0800

    Fixed Company Routing

--- 134b/src/containers/Company/Company.js
+++ 134b/src/containers/Company/Company.js
@@ -1,17 +1,16 @@
 import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 import styles from './Company.css';
 import CompanyAPI from './CompanyAPI';
 import JobOpening from './JobOpening/JobOpening'
-//import CurrentJobPosting from './CurrentJobPosting/CurrentJobPosting';
+import CurrentJobPosting from './CurrentJobPosting/CurrentJobPosting';
 
 class Company extends Component {
     
     state = {
         currCompany: null,
         userType: '',
-        renderPositions: false,
         bannerPath: ""
     }
 
@@ -20,8 +19,6 @@ class Company extends Component {
         let retrievedCompanies = CompanyAPI.all();
         console.log(retrievedCompanies);
         //console.log(this.props.match.params);
-        if(this.props.match.params.positionId != null)
-            this.setState({renderPositions: true})
         const companyId = this.props.match.params.companyId;
         for (var i = 0; i < retrievedCompanies.length; i++){
             if(companyId == retrievedCompanies[i].id){
@@ -33,30 +30,25 @@ class Company extends Component {
     }
 
     render(){
-        if(!this.state.renderPositions){
-            //bannerPath = "'../../Shared/Images/" + this.state.currCompany.bannerURL + "'";
-            //style={{backgroundImage: "url("+ require(this.state.bannerPath) + ")" }}
-            return (
-                <div>
-                    <div className="jumbotron jumbotron-fluid">
-                    </div>
-                    <div className="open-jobs">
-                        <div id ="id-about" style={{ paddingLeft: '1%'}}></div>
-                        <hr></hr>
-                        <div style={{paddingLeft:'1%'}}>
-                            <JobOpening positions={this.state.currCompany.openPositions}/>
-                        </div>
-                        <div style={{paddingLeft:'2%'}}>
-                            {/* <button id="btn-add" onClick="addNewPosition(1)" className="btn btn-success" type="button" style="float: left">Add</button> */}
-                        </div>   
-                    </div>
+        //bannerPath = "'../../Shared/Images/" + this.state.currCompany.bannerURL + "'";
+        //style={{backgroundImage: "url("+ require(this.state.bannerPath) + ")" }}
+        return (
+            <div>
+                <div className="jumbotron jumbotron-fluid">
                 </div>
-            );
-        } else {
-            return (<div>
-                        <Route path="/company/:companyId/position/:positionId"/>
-                    </div>);
-        }
+                <div className="open-jobs">
+                    <div id ="id-about" style={{ paddingLeft: '1%'}}></div>
+                    <hr></hr>
+                    <div style={{paddingLeft:'1%'}}>
+                        <JobOpening companyId={this.state.currCompany.id}
+                            positions={this.state.currCompany.openPositions}/>
+                    </div>
+                    <div style={{paddingLeft:'2%'}}>
+                        {/* <button id="btn-add" onClick="addNewPosition(1)" className="btn btn-success" type="button" style="float: left">Add</button> */}
+                    </div>   
+                </div>
+            </div>
+        ); 
     } 
 }
 
---
+++ 134b/src/containers/Company/CurrentJobPosting/CurrentJobPosting.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import Company from '../Company'
+
+class CurrentJobPosting extends Component {
+    render() {
+        return(
+            <h1>Test!</h1>
+        )
+    }
+}
+
+export default CurrentJobPosting;
