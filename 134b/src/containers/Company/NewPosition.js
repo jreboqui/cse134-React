89d344895a648c0da125de24a695c67713f39f96
Melvin Wijaya <mewijaya@ucsd.edu>
2018-03-04 12:40:15 -0800

    NewPosition.js finished

--- 134b/src/containers/Company/NewPosition.js
+++ 134b/src/containers/Company/NewPosition.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { browserRouter, Link, Route } from 'react-router-dom';
 import CompanyAPI from '../Company/CompanyAPI';
+import Position from '../../Shared/Position';
 
 class NewPosition extends Component {
     constructor(props) {
@@ -32,11 +33,21 @@ class NewPosition extends Component {
     }
 
     handleCancel = () => {
-        //this.props.history.push('/student/' + this.props.location.pathname[this.props.location.pathname.length-1]);
+        this.props.history.push('/company/' + this.props.match.params.companyId);
     }
 
-    handleSubmit = () => {
-    
+    handlePost = (event) => {
+        const company = CompanyAPI.get(
+            parseInt(this.props.match.params.companyId, 10)
+        )
+        console.log(company);
+
+        const newPosition = new Position(company.openPositions.length + 1, this.state.title, this.state.location,
+                                         this.state.description, this.state.requirements, "Under Review", '');
+        CompanyAPI.addPosition(parseInt(this.props.match.params.companyId),newPosition);
+        event.preventDefault();
+        this.props.history.push('/company/' + this.props.match.params.companyId);
+        console.log(company);
     }
 
 
@@ -51,14 +62,14 @@ class NewPosition extends Component {
 			        <label for="title">Job Title:</label>
 			        <input name="title" type="text" placeholder="Position.." value={this.state.title} onChange={this.handleChange} />
 
-                    <label for="city">Location:</label>
-                    <textarea name="city" class="jobcity" value={this.state.location} onChange={this.handleChange} />
+                    <label for="location">Location:</label>
+                    <textarea name="location" class="job-location" value={this.state.location} onChange={this.handleChange} />
 			    
 			        <label for="description">Job Description:</label>
-			        <textarea name="description" class="jobdescription" placeholder="Enter job description here.." value={this.state.description} onChange={this.handleChange} />
+			        <textarea name="description" class="job-description" placeholder="Enter job description here.." value={this.state.description} onChange={this.handleChange} />
 
-			        <label for="requirement">Requirements</label>
-			        <textarea name="requirement" id="jobrequirement" value={this.state.requirements} onChange={this.handleChange} />
+			        <label for="requirements">Requirements</label>
+			        <textarea name="requirements" id="job-requirement" value={this.state.requirements} onChange={this.handleChange} />
 			
                     <input type="reset" value="Reset" onClick={this.handleReset} />
                     <button class="button-cancel" type="button" onClick={this.handleCancel}>Cancel</button>
