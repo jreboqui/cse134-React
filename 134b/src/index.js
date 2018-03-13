f01a1826ced9cf7db332fa0d43d7c212331004c1
Kevin Pansawira <kpansawi@ucsd.edu>
2018-03-13 14:37:14 -0700

    Added Store and companyData Reducer

--- 134b/src/index.js
+++ 134b/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import companyDataReducer from './store/reducers/companyData';
+
+const rootReducer = combineReducers({
+    companyData: companyDataReducer
+    //Combine all reducers here..
+});
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
