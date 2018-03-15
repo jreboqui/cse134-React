a2b0a0f92c442862acf8273ba7070e865b79354a
Kevin Pansawira <kpansawi@ucsd.edu>
2018-03-15 04:03:16 -0700

    Cleaned up index.js

--- 134b/src/index.js
+++ 134b/src/index.js
@@ -5,10 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from 'react-redux';
-import {createStore, combineReducers} from 'redux';
-import {loadCourses} from './actions/courseActions';
-import {loadAuthors} from './actions/authorActions';
-//import companyDataReducer from './store/reducers/companyData';
 import configureStore from './store/configureStore';
 import { loadStudents } from './actions/studentActions';
 import { loadCompanies } from './actions/companyActions';
@@ -20,8 +16,6 @@ import { loadCompanies } from './actions/companyActions';
 //const store = createStore(rootReducer);
 
 const store = configureStore();
-//store.dispatch(loadCourses());
-//store.dispatch(loadAuthors());
 store.dispatch(loadStudents());
 store.dispatch(loadCompanies());
 
